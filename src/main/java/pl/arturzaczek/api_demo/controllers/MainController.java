66915827c59eb2eab510d2518181f1c40578bceb
arturzaczek1 <artur.zaczek@tlen.pl>
2020-09-22 18:41:00 +0200

    initial commit

---
+++ src/main/java/pl/arturzaczek/api_demo/controllers/MainController.java
@@ -0,0 +1,20 @@
+package pl.arturzaczek.api_demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/home")
+    public String getHome (){
+        System.out.println("getHome()");
+        return "home jason";
+    }
+
+    @GetMapping("/1")
+    public ResponseEntity getTestController() {
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
